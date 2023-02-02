6ca69fdaebb7a4a3ffde722ade335cf38e23c15c
Sebastian Meckovski <Sebastian.Meckovski@streets-heaver.com>
2023-02-02 15:39:52 +0000

    import deleted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import CurrencyConverter from './CurrencyConverter';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
